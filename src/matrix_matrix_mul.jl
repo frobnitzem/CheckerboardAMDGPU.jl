caea83be736ba0aad941c33b2cfaafc033b73711
David M. Rogers <predictivestatmech@gmail.com>
2023-07-06 21:06:33 -0400

    Added tests and fixed a bug.

--- src/matrix_matrix_mul.jl
+++ src/matrix_matrix_mul.jl
@@ -5,80 +5,78 @@
 Continuous = Union{AbstractFloat, Complex{<:AbstractFloat}}
 
 #function color_rmul_kernel!(A::AMDGPU.Device.ROCDeviceMatrix{Float32, 1},
-#					start::Int32, stop::Int32,
-#					nt::AMDGPU.Device.ROCDeviceMatrix{Int32, 1},
-#					ch::AMDGPU.Device.ROCDeviceVector{Float64, 1},
-#					sh::AMDGPU.Device.ROCDeviceVector{Float64, 1},
-#					inverse::Int32)
+#                    start::Int32, stop::Int32,
+#                    nt::AMDGPU.Device.ROCDeviceMatrix{Int32, 1},
+#                    ch::AMDGPU.Device.ROCDeviceVector{Float64, 1},
+#                    sh::AMDGPU.Device.ROCDeviceVector{Float64, 1},
+#                    inverse::Int32)
 function color_rmul_kernel!(A, start, stop, nt, ch, sh, inverse)
     i0 = AMDGPU.blockIdx().x+start-1
-	Ni = AMDGPU.gridGroupDim().x # gridDim
+    Ni = AMDGPU.gridGroupDim().x # gridDim
         
     k0 = AMDGPU.threadIdx().x
     Nk = AMDGPU.blockDim().x
 
-	# causes terrible compiler error:
-	#for n = i0:Ni:stop
-	n = i0
-	while n <= stop
-		i = nt[1,n]
-		j = nt[2,n]
-		cij = ch[n]
-		sij = inverse * sh[n]
-		#for k = k0:Nk:size(A,1)
-		k = k0
-		while k <= size(A,1)
-			vi = A[k,i]
-			vj = A[k,j]
-			vi = cij*vi + conj(sij)*vj
-			vj = sij*vi + cij*vj
-			A[k,i] = vi
-			A[k,j] = vj
-			k += Nk
-		end
-		n += Ni
+    # causes terrible compiler error:
+    #for n = i0:Ni:stop
+    n = i0
+    while n <= stop
+        i = nt[1,n]
+        j = nt[2,n]
+        cij = ch[n]
+        sij = inverse * sh[n]
+        #for k = k0:Nk:size(A,1)
+        k = k0
+        while k <= size(A,1)
+            vi = A[k,i]
+            vj = A[k,j]
+            A[k,i] = cij*vi + conj(sij)*vj
+            A[k,j] = sij*vi + cij*vj
+            k += Nk
+        end
+        n += Ni
     end
 
     return
 end
 
 function color_rmul!(A::AMDGPU.ROCMatrix{T},
-		start::Int32, stop::Int32,
-		neighbor_table::AMDGPU.ROCMatrix{Int32},
+        start::Int32, stop::Int32,
+        neighbor_table::AMDGPU.ROCMatrix{Int32},
         coshΔτt::AMDGPU.ROCVector{E}, sinhΔτt::AMDGPU.ROCVector{E};
-		inverted::Bool = false) where {T<:Continuous, E<:Continuous}
+        inverted::Bool = false) where {T<:Continuous, E<:Continuous}
 
     @assert !(T<:Real && E<:Complex) "Cannot multiply a real valued matrix by complex a checkerboard matrix!"
 
-	#https://amdgpu.juliagpu.org/stable/kernel_launch/
-	#kernel = @roc launch=false mykernel(args...)
-	#@show AMDGPU.Compiler.calculate_occupancy(kernel.fun, AMDGPU.default_device())
+    #https://amdgpu.juliagpu.org/stable/kernel_launch/
+    #kernel = @roc launch=false mykernel(args...)
+    #@show AMDGPU.Compiler.calculate_occupancy(kernel.fun, AMDGPU.default_device())
 
-	# q = AMDGPU.ROCQueue()
-	# queue = q
-	inverse::Int32 = 1 - 2*inverted
+    # q = AMDGPU.ROCQueue()
+    # queue = q
+    inverse::Int32 = 1 - 2*inverted
 
-	blocks = min(220, stop-start+1)
-	threads = 64
-	#println("Starting kernel ", start, ":", stop)
-	#@show size(A)
-	#@show size(neighbor_table)
-	#@show size(coshΔτt)
-	#@show size(sinhΔτt)
-	return AMDGPU.@roc threads = threads blocks = blocks color_rmul_kernel!(
-								A, start, stop, neighbor_table,
-								coshΔτt, sinhΔτt, inverse)
+    blocks = min(220, stop-start+1)
+    threads = 64
+    #println("Starting kernel ", start, ":", stop)
+    #@show size(A)
+    #@show size(neighbor_table)
+    #@show size(coshΔτt)
+    #@show size(sinhΔτt)
+    return AMDGPU.@roc threads = threads blocks = blocks color_rmul_kernel!(
+                                A, start, stop, neighbor_table,
+                                coshΔτt, sinhΔτt, inverse)
 end
 
 function checkerboard_rmul!(A::AMDGPU.ROCMatrix{T},
-		neighbor_table::AMDGPU.ROCMatrix{Int32},
+        neighbor_table::AMDGPU.ROCMatrix{Int32},
         coshΔτt::AMDGPU.ROCVector{E}, sinhΔτt::AMDGPU.ROCVector{E},
         colors::AbstractMatrix{Int32};
-		transposed::Bool=false, inverted::Bool=false) where {T<:Continuous, E<:Continuous}
+        transposed::Bool=false, inverted::Bool=false) where {T<:Continuous, E<:Continuous}
 
     @assert !(T<:Real && E<:Complex) "Cannot multiply a real valued matrix by complex a checkerboard matrix!"
 
-	# number of checkerboard colors
+    # number of checkerboard colors
     Ncolors = size(colors, 2)
 
     # how to iterate over neighbors in neighbor_table accounting for whether
@@ -88,15 +86,16 @@ function checkerboard_rmul!(A::AMDGPU.ROCMatrix{T},
     step       = 1 - 2*transposed
     stop       = (1-transposed)*Ncolors + transposed
 
-	event = nothing
-	for color in start:step:stop
-		event = color_rmul!(A, colors[1,color], colors[2,color],
-							neighbor_table, coshΔτt, sinhΔτt, inverted=inverted)
-	end
-	# Events run in-order, so it suffices to wait for the last one.
-	if event != nothing
-		AMDGPU.wait(event)
-	end
+    event = nothing
+    for color in start:step:stop
+        event = color_rmul!(A, colors[1,color], colors[2,color],
+                            neighbor_table, coshΔτt, sinhΔτt;
+                            inverted=inverted)
+    end
+    # Events run in-order, so it suffices to wait for the last one.
+    if event != nothing
+        AMDGPU.wait(event)
+    end
 
-	return nothing
+    return nothing
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using CheckerboardAMDGPU
+using AMDGPU
+
 using Checkerboard
 using LinearAlgebra
 using Test
@@ -11,58 +13,58 @@ using Test
 	t  = 1.0
 	Δτ = 0.1
     #Γ = CheckerboardMatrix(nt, t, Δτ)
-	nt = [1 3 5 7 9 11 13 15 4 2 8 6 12 10 16 14 1 2 3 4 9 10 11 12 13 14 15 16 5 6 7 8;
+	neighbor_table = [1 3 5 7 9 11 13 15 4 2 8 6 12 10 16 14 1 2 3 4 9 10 11 12 13 14 15 16 5 6 7 8;
 		  2 4 6 8 10 12 14 16 1 3 5 7 9 11 13 15 5 6 7 8 13 14 15 16 1 2 3 4 9 10 11 12]
-	coshΔτt = fill(cosh(t*Δτ), size(nt,2))
-	sinhΔτt = fill(sinh(t*Δτ), size(nt,2))
-	colors = [1 9 17 25;
-			  8 16 24 32]
+
+	coshΔτt = fill(cosh(t*Δτ), size(neighbor_table,2))
+	sinhΔτt = fill(sinh(t*Δτ), size(neighbor_table,2))
+	colors = Matrix{Int32}(
+		     [ 1  9 17 25;
+			   8 16 24 32])
 
     # calculate exact exponentiated hopping matrix exp(-Δτ⋅K)
 	K = zeros(Float64, N, N)
-    for c in axes(nt,2)
-        i      = nt[1,c]
-        j      = nt[2,c]
+    for c in axes(neighbor_table,2)
+		# Note: exp([[0,t], [t*, 0]]) = [[c, s], [s*, c]]
+		# where c = cosh(|t|), s = sinh(|t|) t/|t|
+        i      = neighbor_table[1,c]
+        j      = neighbor_table[2,c]
         K[j,i] = -t
         K[i,j] = conj(-t)
     end
     expnΔτK = Hermitian(exp(-Δτ*K))
 
-    # test vector multiplication
-    v  = randn(N)
-    v′ = copy(v)
-	checkerboard_lmul!(v′, nt, coshΔτt, sinhΔτt, colors)
-	
-    u = zeros(N)
-	mul!(u, expnΔτK, v)
-	@test norm(v′-u)/norm(u) < Δτ*0.5
-
-	checkerboard_lmul!(v′, nt, coshΔτt, sinhΔτt, colors; inverted=true)
-    @test v′ ≈ v
+	nt = AMDGPU.ROCArray{Int32,2}(neighbor_table)
+	ch = AMDGPU.ROCArray{Float64,1}(coshΔτt)
+	sh = AMDGPU.ROCArray{Float64,1}(sinhΔτt)
 
 #   # build dense versions of matrices
-#   I_dense   = Matrix{Float64}(I,L^2,L^2)
-#   Γ_dense   = similar(I_dense)
-#   Γᵀ_dense  = similar(I_dense)
-#   Γ⁻¹_dense = similar(I_dense)
-#   mul!(Γ_dense,   Γ,   I_dense)
-#   mul!(Γᵀ_dense,  Γᵀ,  I_dense)
-#   mul!(Γ⁻¹_dense, Γ⁻¹, I_dense)
+    I_dense   = Matrix{Float64}(I,L^2,L^2)
+    Γ_dense   = AMDGPU.ROCMatrix{Float64}(I_dense)
+    Γᵀ_dense  = AMDGPU.ROCMatrix{Float64}(I_dense)
+    Γ⁻¹_dense = AMDGPU.ROCMatrix{Float64}(I_dense)
+	CheckerboardAMDGPU.checkerboard_rmul!(Γ_dense,  nt, ch, sh,
+										  colors; transposed=false, inverted=false)
+    CheckerboardAMDGPU.checkerboard_rmul!(Γᵀ_dense, nt, ch, sh,
+										  colors; transposed=true, inverted=false)
+    CheckerboardAMDGPU.checkerboard_rmul!(Γ⁻¹_dense,nt, ch, sh,
+										  colors; transposed=false, inverted=true)
 
-#   @test Γ_dense ≈ transpose(Γᵀ_dense)
-#   @test Γ_dense ≈ inv(Γ⁻¹_dense)
-#   @test norm(Γ_dense-expnΔτK)/norm(expnΔτK) < 0.01
+	A   = Matrix{Float64}(undef,L^2,L^2)
+	copyto!(A, Γ_dense)
+	@show view(A, 1:3, 1:3)
+    @test norm(A-expnΔτK)/norm(expnΔτK) < 0.02001
 
-#   A = similar(Γ_dense)
-#   B = similar(Γ_dense)
-#   C = similar(Γ_dense)
+	Aᵀ  = Matrix{Float64}(undef,L^2,L^2)
+	copyto!(Aᵀ, Γᵀ_dense)
+	@show view(Aᵀ, 1:3, 1:3)
+	@test norm(A - transpose(Aᵀ)) < 1e-6
 
-#   mul!(A,Γ,I_dense)
-#   mul!(B,I_dense,Γ)
-#   @test A ≈ B
+	A⁻¹ = Matrix{Float64}(undef,L^2,L^2)
+	copyto!(A⁻¹, Γ⁻¹_dense)
 
-#   @. C = rand()
-#   mul!(A,Γ,C)
-#   mul!(B,C,Γ)
-#   @test !(A ≈ B)
+	result = Matrix{Float64}(undef,L^2,L^2)
+	mul!(result, A⁻¹, A)
+	@test norm(I_dense - result) < 1e-6
+	#@test A⁻¹ ≈ inv(A)
 end
